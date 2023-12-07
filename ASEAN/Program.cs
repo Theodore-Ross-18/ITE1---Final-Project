a23bf2d265427de8e2e5a0d124d0b5950957545f
Theodore Ross <theodoreross18@gmail.com>
2023-12-07 10:28:00 +0800

    Main Class and Main Method

--- ASEAN/Program.cs
+++ ASEAN/Program.cs
@@ -1,1 +1,12 @@
-﻿// hello world
+﻿
+// Main: Program Class
+public class Program 
+{
+    // Main Method: Program Starts Execution
+    static void Main() 
+    {
+        // Creating: Instance of the ASEAN Phonebook Class
+        ASEANPhonebook aseanPhonebook = new ASEANPhonebook();
+    }
+
+}
