aa75c48763ed91844347f7bcef646543b5138526
Theodore Ross <theodoreross18@gmail.com>
2023-12-07 10:48:07 +0800

    Switch: Case 1

--- ASEAN/Program.cs
+++ ASEAN/Program.cs
@@ -17,6 +17,33 @@ public class Program
             Console.Write("\nEnter Choice: ");
             string choice = Console.ReadLine();
 
+            // Switching: Based on User Choices
+            switch (choice)
+            {
+                case "1":
+                    // Adding: Students to the Phonebook
+                    while (true)
+                    {
+                        Console.Write("\nEnter Number: ");
+                        string studentNumber = Console.ReadLine();
+
+                        Console.Write("Enter Surname: ");
+                        string surname = Console.ReadLine();
+
+                        Console.Write("Enter First Name: ");
+                        string firstName = Console.ReadLine();
+
+                        Console.Write("Enter Occupation: ");
+                        string occupation = Console.ReadLine();
+
+                        Console.Write("Enter Gender (M for male, F for female): "):
+                        string genderInput = Console.ReadLin().ToUpper();
+
+                        // Conditional: Gender Input
+
+                    }
+            }
+
         }
 
 
