4990957fe23d3dfc583648f653ca8742b404072d
Theodore Ross <theodoreross18@gmail.com>
2023-12-07 10:39:41 +0800

    User Input: Choices

--- ASEAN/Program.cs
+++ ASEAN/Program.cs
@@ -7,6 +7,19 @@ public class Program
     {
         // Creating: Instance of the ASEAN Phonebook Class
         ASEANPhonebook aseanPhonebook = new ASEANPhonebook();
+
+        while (true)
+        {
+            // User Interface: The Main Menu
+            Console.WriteLine("\nMAIN MENU\n[1] Store to ASEAN phonebook\n[2] Edit entry in ASEAN phonebook\n" +
+            "[3] Search ASEAN phonebook by country\n[4] Exit");
+
+            Console.Write("\nEnter Choice: ");
+            string choice = Console.ReadLine();
+
+        }
+
+
     }
 
 }
